b1e37d01e085894ec482894ff6e56c6f10e2f274
Jinyang Zhou <pandachow19910411@gmail.com>
2016-06-03 01:32:29 +0800

    fix

--- senti.py
+++ senti.py
@@ -2,7 +2,7 @@ import goslate
 import os
 #  from matplotlib import pyplot,pylab as plt
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
-import numpy as np
+#  import numpy as np
 #  import cv2
 from rumor import RumorJudges, PreLoading, Add
 import Image
@@ -39,21 +39,11 @@ def multi():
     file1 = uploaded_files[0]
     file2 = uploaded_files[1]
 
-    img_rgb = cv2.imread('uploads/'+file1.filename)
+    #  img_rgb = cv2.imread('uploads/'+file1.filename)
+
     print('uploads/'+file1.filename)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread('uploads/'+file2.filename,0)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where( res >= threshold)
-    for pt in zip(*loc[::-1]):
-      cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
 
-    im = Image.fromarray(img_rgb)
-    im.save('uploads/tm'+filename)
 
     print(type(img_rgb))
 
@@ -64,16 +54,16 @@ def multi():
 def upload():
     file = request.files['file']
     if file and allowed_file(file.filename):
-	filename = file.filename
+        filename = file.filename
         file.save(os.path.join(senti.config['UPLOAD_FOLDER'], filename))
-        img = cv2.imread('uploads/'+filename)
+        print filename
+        #  img = cv2.imread('uploads/'+filename)
 
-        if 'Denoise Image' in request.form['submit']:
-		dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-		im = Image.fromarray(dst)
-		im.save('uploads/d'+filename)
-       	 	return redirect(url_for('uploaded_file',filename="d"+filename))
-	elif 'Detect Face' in request.form['submit']:
+    if 'Denoise Image' in request.form['submit']:
+        print "hahah"
+        return redirect(url_for('uploaded_file',filename="d"+filename))
+
+    elif 'Detect Face' in request.form['submit']:
 		faceCascade = cv2.CascadeClassifier('static/xml/haarcascade_frontalface_alt.xml')
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		faces = faceCascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(30, 30),flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
@@ -82,13 +72,13 @@ def upload():
 		im = Image.fromarray(img)
 		im.save('uploads/face'+filename)
 		return redirect(url_for('uploaded_file',filename="face"+filename))
-	elif 'Smoothen Image' in request.form['submit']:
+    elif 'Smoothen Image' in request.form['submit']:
 		kernel = np.ones((5,5),np.float32)/25
 		dst = cv2.filter2D(img,-1,kernel)
 		im = Image.fromarray(dst)
 		im.save('uploads/smooth'+filename)
 		return redirect(url_for('uploaded_file',filename="smooth"+filename))
-	elif 'Detect Blue Colour' in request.form['submit']:
+    elif 'Detect Blue Colour' in request.form['submit']:
 		# Convert BGR to HSV
 		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		# define range of blue color in HSV
@@ -101,12 +91,6 @@ def upload():
 		im = Image.fromarray(res)
 		im.save('uploads/blue'+filename)
 		return redirect(url_for('uploaded_file',filename="blue"+filename))
-	elif 'Image Resize' in request.form['submit']:
-	   	small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-	   	resized_image = cv2.resize(image, (100, 50))
-      		small = scipy.misc.imresize(image, 0.5)
-      		print(type(small))
-      		return ''
 
 @senti.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -153,4 +137,4 @@ def translate():
     return jsonify(result=gs.translate(textVal,langVal))
 
 if __name__ == "__main__":
-    senti.run('0.0.0.0', debug=True)
+    senti.run('0.0.0.0', debug=True, port=1111)
